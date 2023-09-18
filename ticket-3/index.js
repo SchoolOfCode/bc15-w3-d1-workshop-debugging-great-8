7d3ab265f9f451cf2a22974a06e23a35e42a1153
“Katie-W-22” <katiecoder22@gmail.com>
2023-09-18 16:58:26 +0100

    we don't like ticket 3

--- ticket-3/index.js
+++ ticket-3/index.js
@@ -1,14 +1,18 @@
-function countNumberOfNinesInNumber(number) {
-  let count = 0;
 
+
+function countNumberOfNinesInNumber(number) {
+  const numberString = number.toString();
+
+  let count = 0;
   for (let i = 0; i < number.length; i++) {
     const digit = number[i];
-    if (9 == digit) {
+    if ("9" === digit) {
       count++;
     }
+    
   }
-
   return count;
 }
 
-countNumberOfNinesInNumber(98789);
+
+console.log(countNumberOfNinesInNumber(98789));
